8064d9c73250886bae7de8b039d7eafdd91ce02d
asolanky <alwinsolanky@gmail.com>
2020-04-09 17:15:52 +0100

    Card types finished

--- card.js
+++ card.js
@@ -9,6 +9,18 @@ function validate(number) {
     if( begin.substring(0,1) == 4 && ( length == 13 || length == 16 ) ){
         return 'VISA';
     }
+    else if( number.toString().substring(0,4) == 6011 && length == 16 )
+    {
+        return 'Discover';
+    }
+    else if( ( begin >= 51 && begin <= 55 ) && length == 16 )
+    {
+        return 'Mastercard';
+    }
+    else if( ( begin == 34 || begin == 37 ) && length == 15 )
+    {
+        return 'AMEX';
+    }
     else {
       return 'Unknown';
     }
--- card.test.js
+++ card.test.js
@@ -7,3 +7,15 @@ test('VISA: 4111111111111111 (valid)', () => {
 test('Unkown: 22222222 (invalid)', () => {
   expect(validate(22222222)).toEqual('Unknown');
 });
+
+test('Discover: 6011111111111117 (valid)', () => {
+  expect(validate(6011111111111117)).toEqual('Discover');
+});
+
+test('Mastercard: 5105105105105100 (valid)', () => {
+  expect(validate(5105105105105100)).toEqual('Mastercard');
+});
+
+test('AMEX: 378282246310005 (valid)', () => {
+  expect(validate(378282246310005)).toEqual('AMEX');
+});
